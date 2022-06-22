69057593d9824cf09819542b680cc91483a5577a
CHO Min Ho <chominho96@inha.edu>
2022-06-22 20:39:02 +0900

    [기능 추가] Creation 클래스 추가
    1. Creation 클래스 추가
    2. CreationRepository 클래스 추가

---
+++ moai/src/main/java/MOAI/moai/creation/Creation.java
@@ -0,0 +1,36 @@
+package MOAI.moai.creation;
+
+import MOAI.moai.common.BaseEntity;
+import MOAI.moai.member.Member;
+import MOAI.moai.music.Music;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Creation extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ctn_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "msc_id")
+    private Music music;
+
+    @OneToOne
+    @JoinColumn(name = "mbr_id")
+    private Member artist;
+
+    @Column(name = "ctn_name")
+    private String name;
+
+    @Column(name = "ctn_file_url")
+    private String fileUrl;
+
+    @Column(name = "ctn_like_num")
+    private Integer likeNum;
+
+}
---
+++ moai/src/main/java/MOAI/moai/creation/repository/CreationRepository.java
@@ -0,0 +1,7 @@
+package MOAI.moai.creation.repository;
+
+import MOAI.moai.creation.Creation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreationRepository extends JpaRepository<Creation, Long> {
+}
