27ff47906ff029fdefcd540ed493c0eb59da6ef2
CHO Min Ho <chominho96@inha.edu>
2022-06-22 21:10:24 +0900

    [기능추가] MusicRequest 클래스 추가
    1. MusicRequest 클래스 추가
    2. MusicRequestRepository 클래스 추가

--- moai/src/main/java/MOAI/moai/music/MusicRequest.java
+++ moai/src/main/java/MOAI/moai/music/MusicRequest.java
@@ -1,17 +1,38 @@
 package MOAI.moai.music;
 
 import MOAI.moai.common.BaseEntity;
+import MOAI.moai.member.Member;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
 public class MusicRequest extends BaseEntity {
 
     @Id
     @GeneratedValue
     @Column(name = "req_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "msc_id")
+    private Music music;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mbr_id")
+    private Member member;
+
+    @Column(insertable = false, updatable = false)
+    private String dtype;
+
+    @Column(name = "req_succ")
+    @ColumnDefault("false")
+    private Boolean isSuccess;
+
+
 
 }
---
+++ moai/src/main/java/MOAI/moai/music/repository/MusicRequestRepository.java
@@ -0,0 +1,8 @@
+package MOAI.moai.music.repository;
+
+import MOAI.moai.music.MusicRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MusicRequestRepository extends JpaRepository<MusicRequest, Long> {
+
+}
