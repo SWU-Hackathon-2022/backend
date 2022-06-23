d5331d77446d190f62742eddcb1a49271cd77b38
SeungKeun Choi <77659341+ddackkeun@users.noreply.github.com>
2022-06-23 15:07:11 +0900

    [기능추가] Creation, Note 테스트 추가 1. Creation 테스트데이터 추가 2. Note 테스트데이터 추가

--- moai/src/main/java/MOAI/moai/test_data/TestData.java
+++ moai/src/main/java/MOAI/moai/test_data/TestData.java
@@ -1,5 +1,7 @@
 package MOAI.moai.test_data;
 
+import MOAI.moai.creation.Creation;
+import MOAI.moai.creation.repository.CreationRepository;
 import MOAI.moai.member.Member;
 import MOAI.moai.member.MemberGenre;
 import MOAI.moai.member.controller.MemberController;
@@ -10,6 +12,9 @@ import MOAI.moai.member.service.MemberService;
 import MOAI.moai.member.type.MemberType;
 import MOAI.moai.music.Music;
 import MOAI.moai.music.repository.MusicRepository;
+import MOAI.moai.note.Note;
+import MOAI.moai.note.repository.NoteRepository;
+import MOAI.moai.note.type.NoteType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -37,6 +42,8 @@ public class TestData {
         private final MemberRepository memberRepository;
         private final MusicRepository musicRepository;
         private final MemberGenreRepository memberGenreRepository;
+        private final CreationRepository creationRepository;
+        private final NoteRepository noteRepository;
 
         @Transactional
         public void init() {
@@ -88,6 +95,31 @@ public class TestData {
             musicRepository.save(music4);
             musicRepository.save(music5);
 
+            // Creation 추가
+            Creation creation1 = Creation.createCreation(music1, member3, "영상이름 1",null );
+            Creation creation2 = Creation.createCreation(music2, member3, "영상이름 2",null );
+            Creation creation3 = Creation.createCreation(music3, member3, "영상이름 3",null );
+            Creation creation4 = Creation.createCreation(music4, member3, "영상이름 4",null );
+            Creation creation5 = Creation.createCreation(music5, member3, "영상이름 5",null );
+            creationRepository.save(creation1);
+            creationRepository.save(creation2);
+            creationRepository.save(creation3);
+            creationRepository.save(creation4);
+            creationRepository.save(creation5);
+
+            // Note 추가
+            Note note1 = Note.createNote(music1, member3, null, NoteType.ARTIST, "안녕하세요 제 이름은 김아티스트입니다. 같이 협업 할 수 있을까요. 이 음악을 사용할 수 있게 해주세요.");
+            Note note2 = Note.createNote(music1, member4, null, NoteType.ARTIST, "안녕하세요 제 이름은 이아티스트입니다. 같이 협업 할 수 있을까요. 이 음악을 사용할 수 있게 해주세요.");
+            Note note3 = Note.createNote(music1, member5, null, NoteType.ARTIST, "안녕하세요 제 이름은 박아티스트입니다. 같이 협업 할 수 있을까요. 이 음악을 사용할 수 있게 해주세요.");
+            noteRepository.save(note1);
+            noteRepository.save(note2);
+            noteRepository.save(note3);
+            Note note4 = Note.createNote(music1, member1, note1, NoteType.COMPOSER, "안녕하세요 김작곡입니다. 아티스트님은 저희와 함께갑시다.");
+            Note note5 = Note.createNote(music1, member1, note2, NoteType.COMPOSER, "안녕하세요 김작곡입니다. 아티스트님은 아쉽지만 저희와 함께갈 수 없습니다.");
+            Note note6 = Note.createNote(music1, member1, note3, NoteType.COMPOSER, "안녕하세요 김작곡입니다. 아티스트님은 아쉽지만 저희와 함께갈 수 없습니다.");
+            noteRepository.save(note4);
+            noteRepository.save(note5);
+            noteRepository.save(note6);
         }
     }
 
