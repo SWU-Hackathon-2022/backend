4d09027e1622eeef004bb90db9153a66b3a598a8
CHO Min Ho <chominho96@inha.edu>
2022-06-23 15:11:26 +0900

    [기능추가] LoginServiceTest 추가
    1. LoginServiceTest 추가

---
+++ moai/src/test/java/MOAI/moai/login/service/LoginServiceTest.java
@@ -0,0 +1,35 @@
+package MOAI.moai.login.service;
+
+import MOAI.moai.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class LoginServiceTest {
+
+    @Test
+    void login() throws Exception {
+        /**
+         *  given : given condition
+         */
+        //Member member = Member.createMember("COMPOSER", )
+
+
+        /**
+         *  when  : execution
+         */
+
+
+
+        /**
+         *  then  : result
+         */
+
+
+    }
+}
